363b2ce023818a7d3b0542c4dfa544d8dbafdafd
Tim Chung <31941027+timhyc19@users.noreply.github.com>
2023-09-07 13:05:19 -0400

    Fix again

--- pages/experience/0.js
+++ pages/experience/0.js
@@ -1,27 +1,27 @@
-// pages/experience/0.js
-import React from 'react';
+// // pages/experience/0.js
+// import React from 'react';
 
-const WorkExperience0 = () => {
-  return (
-    <div className="bg-gray-200 min-h-screen">
-      <div className="px-10 md:px-20 lg:px-40">
-        <h1>Cohere</h1>
-        <p>Software Engineer Intern | Fall 2023</p>
-        <p>Data infra and pipelines</p>
+// const WorkExperience0 = () => {
+//   return (
+//     <div className="bg-gray-200 min-h-screen">
+//       <div className="px-10 md:px-20 lg:px-40">
+//         <h1>Cohere</h1>
+//         <p>Software Engineer Intern | Fall 2023</p>
+//         <p>Data infra and pipelines</p>
 
-        <h2>Summary</h2>
-        <p>
-          During my internship at Cohere, I worked on various aspects of data infrastructure and pipelines. 
-          My responsibilities included designing and optimizing data pipelines, ensuring data quality, and 
-          collaborating with cross-functional teams to support data-driven decision-making processes.
-        </p>
+//         <h2>Summary</h2>
+//         <p>
+//           During my internship at Cohere, I worked on various aspects of data infrastructure and pipelines. 
+//           My responsibilities included designing and optimizing data pipelines, ensuring data quality, and 
+//           collaborating with cross-functional teams to support data-driven decision-making processes.
+//         </p>
 
-        {/* You can add more details or sections as needed */}
-      </div>
-    </div>
-  );
-};
+//         {/* You can add more details or sections as needed */}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default WorkExperience0;
+// export default WorkExperience0;
 
 
